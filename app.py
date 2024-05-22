b41882685994e468eb76906877b41e62aa59c574
joaovictorvasco <168442928+joaovictorvasco@users.noreply.github.com>
2024-05-22 15:06:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 import streamlit as st
 st.title('Fortune Card')
 
-with st.sidebar:
-  st.header('Fortune Card')
-  st.write('Seu aplicativo de cartões')
-  st.caption('Criado por Gabriel, João Victor e Pedro Henrique')
-
 st.write('Nosso app serve para prever estatisticamente o número de cartões numa determinada partida')
-tab_juizes, tab_times = st.tabs(['Juizes', 'Times'])
+tab_juizes, tab_times = st.tabs(['Jogos', 'Juizes', 'Times')
