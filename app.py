fb4dcc546133bb60905ee3e787309a921ce5599c
joaovictorvasco <168442928+joaovictorvasco@users.noreply.github.com>
2024-05-22 15:08:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title('Fortune Card')
 
 st.write('Nosso app serve para prever estatisticamente o número de cartões numa determinada partida')
-tab_juizes, tab_times = st.tabs(['Jogos', 'Juizes', 'Times')
+tab_juizes, tab_times = st.tabs(['Jogos', 'Juizes', 'Times'])
