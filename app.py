7231dcaea621aace37a9a563c4e47550cbb3b5a8
joaovictorvasco <168442928+joaovictorvasco@users.noreply.github.com>
2024-06-09 14:39:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,35 +12,41 @@ st.write("""
 
 # Conteúdo principal
 st.write("""
-    # Nosso app prevê estatisticamente o número de cartões numa determinada partida
+    # Nosso app serve para prever estatisticamente o número de cartões numa determinada partida
 """)
 
-tab_jogos, tab_tabela = st.tabs(['Jogos'], ['Tabela'])
+# Criando abas
+tab1, tab2 = st.tabs(["Jogos", "Tabela"])
 
-times = ['Ath Paranaense', 'Atl Goianiense', 'Atlético Mineiro', 'Bahia', 'Botafogo (RJ)', 
-         'Corinthians', 'Criciúma', 'Cruzeiro', 'Cuiabá', 'Flamengo', 'Fluminense', 
-         'Fortaleza', 'Grêmio', 'Internacional', 'Juventude', 'Palmeiras', 
-         'Red Bull Bragantino', 'São Paulo', 'Vasco da Gama', 'Vitória']
-
-# Ordenando a lista de times
-times_ordenados = sorted(times)
-
-model = XGBRegressor()
-model.load_model('modelo.json') 
-
-with st.form(key='form'):
-    op_home = st.selectbox('Escolha uma opção p/ time da casa', times_ordenados)
-    op_away = st.selectbox('Escolha outra opção p/ time visitante', times_ordenados)
-    enviar = st.form_submit_button('Enviar')
-
-if enviar:
-    if op_home == op_away:
-        st.error('Erro: O mesmo time não pode ser escolhido como time da casa e visitante.')
-    else:
-        lista = [{'Home': op_home, 'Away': op_away, 'ano_x': 2024}]
-        df = pd.DataFrame(lista)
-        df['Home'] = df['Home'].astype('category')
-        df['Away'] = df['Away'].astype('category')
-        resultado = model.predict(df)
+# Conteúdo para a aba "Jogos"
+with tab1:
+    times = ['Ath Paranaense', 'Atl Goianiense', 'Atlético Mineiro', 'Bahia', 'Botafogo (RJ)', 
+             'Corinthians', 'Criciúma', 'Cruzeiro', 'Cuiabá', 'Flamengo', 'Fluminense', 
+             'Fortaleza', 'Grêmio', 'Internacional', 'Juventude', 'Palmeiras', 
+             'Red Bull Bragantino', 'São Paulo', 'Vasco da Gama', 'Vitória']
     
-        st.metric('Nº de cartões predito', int(np.round(resultado[0], 0)))
+    # Ordenando a lista de times
+    times_ordenados = sorted(times)
+    
+    with st.form(key='form'):
+        op_home = st.selectbox('Escolha uma opção p/ time da casa', times_ordenados)
+        op_away = st.selectbox('Escolha outra opção p/ time visitante', times_ordenados)
+        enviar = st.form_submit_button('Enviar')
+    
+    if enviar:
+        if op_home == op_away:
+            st.error('Erro: O mesmo time não pode ser escolhido como time da casa e visitante.')
+        else:
+            lista = [{'Home': op_home, 'Away': op_away, 'ano_x': 2024}]
+            df = pd.DataFrame(lista)
+            df['Home'] = df['Home'].astype('category')
+            df['Away'] = df['Away'].astype('category')
+            model = XGBRegressor()
+            model.load_model('modelo.json') 
+            resultado = model.predict(df)
+            st.write(np.round(resultado, 0))
+
+# Conteúdo para a aba "Tabela"
+with tab2:
+    st.write("Conteúdo da Tabela aqui.")
+
