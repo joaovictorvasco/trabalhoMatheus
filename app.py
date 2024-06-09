9a591e35f67a8e59389a7c630c7863ec5053722c
joaovictorvasco <168442928+joaovictorvasco@users.noreply.github.com>
2024-06-09 19:29:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import streamlit as st
 import pandas as pd
 from xgboost import XGBRegressor
 import numpy as np
-import os
+
+# Inicializando o estado da sessão para controlar a visibilidade da resposta
+if 'show_result' not in st.session_state:
+    st.session_state.show_result = False
 
 sidebar_logo = "fortune.png"
 main_logo = "logo_horizontal.jpg"
@@ -10,7 +13,6 @@ background_color = "#0a1931"
 
 # Sidebar logo display
 st.sidebar.image(sidebar_logo, width=250)
-
 st.sidebar.markdown("""
     <div style="font-size: 20px;"><strong>Aplicativo desenvolvido por:</strong></div>
     <ul style="font-size: 18px; margin-left: 20px;">
@@ -35,14 +37,8 @@ st.markdown("""
     <h2 style="font-size:28px;">Nosso aplicativo utiliza um modelo estatístico para prever o número de cartões em uma determinada partida do Campeonato Brasileiro de 2024.</h2>
 """, unsafe_allow_html=True)
 
-st.markdown("""
-    <h2 style="font-size:18px;">Este modelo analisa dados históricos de confrontos entre times, incluindo estatísticas de jogos anteriores, para calcular a probabilidade de cartões amarelos e vermelhos serem dados durante uma partida. Abaixo, selecione os times da casa e visitante e nosso modelo usará esses dados para prever o resultado provável em termos de cartões.</h2>
-""", unsafe_allow_html=True)
-
-# Tabs for content
 tab1, tab2 = st.tabs(["Jogos", "Tabela"])
 
-# Tab content
 with tab1:
     times = ['Ath Paranaense', 'Atl Goianiense', 'Atlético Mineiro', 'Bahia', 'Botafogo (RJ)', 
              'Corinthians', 'Criciúma', 'Cruzeiro', 'Cuiabá', 'Flamengo', 'Fluminense', 
@@ -50,41 +46,40 @@ with tab1:
              'Red Bull Bragantino', 'São Paulo', 'Vasco da Gama', 'Vitória']
     times_ordenados = sorted(times)
     
-    with st.form(key='form'):
-        op_home = st.selectbox('Escolha uma opção p/ time da casa', times_ordenados)
-        op_away = st.selectbox('Escolha outra opção p/ time visitante', times_ordenados)
+    with st.form(key='prediction_form'):
+        op_home = st.selectbox('Escolha uma opção para o time da casa', times_ordenados)
+        op_away = st.selectbox('Escolha outra opção para o time visitante', times_ordenados)
         enviar = st.form_submit_button('Enviar')
-    
-if enviar:
-    if op_home == op_away:
-        st.error('Erro: O mesmo time não pode ser escolhido como time da casa e visitante.')
-    else:
-        lista = [{'Home': op_home, 'Away': op_away, 'ano_x': 2024}]
-        df = pd.DataFrame(lista)
-        df['Home'] = df['Home'].astype('category')
-        df['Away'] = df['Away'].astype('category')
-        model = XGBRegressor()
-        model.load_model('modelo.json')
-        ypred = model.predict(df)
         
-        # Dados de exemplo dos resultados do modelo
-        r2 = -0.0608767415057232
-        mae = 2.1616382956885682
-        mse = 7.511824680006472
+        if enviar:
+            st.session_state.show_result = True
+            if op_home == op_away:
+                st.error('Erro: O mesmo time não pode ser escolhido como time da casa e visitante.')
+            else:
+                lista = [{'Home': op_home, 'Away': op_away, 'ano_x': 2024}]
+                df = pd.DataFrame(lista)
+                df['Home'] = df['Home'].astype('category')
+                df['Away'] = df['Away'].astype('category')
+                model = XGBRegressor()
+                model.load_model('modelo.json')
+                ypred = model.predict(df)
+                
+                r2 = -0.0608767415057232
+                mae = 2.1616382956885682
+                mse = 7.511824680006472
 
-        st.markdown(f"""
-            ### Previsão de Cartões para a Partida
-            **Confronto:** {op_home} vs {op_away}
-            **Previsão de Cartões Total:** {int(np.round(ypred[0], 0))}
+                st.markdown(f"""
+                    ### Previsão de Cartões para a Partida
+                    **Confronto:** {op_home} vs {op_away}
+                    **Previsão de Cartões Total:** {int(np.round(ypred[0], 0))}
 
-            **Precisão do Modelo:**
-            - \( R^2 \): {r2:.2f} (Um valor negativo indica que o modelo não tem poder preditivo adequado.)
-            - Erro Absoluto Médio (MAE): {mae:.2f}
-            - Erro Quadrático Médio (MSE): {mse:.2f}
+                    **Precisão do Modelo:**
+                    - \( R^2 \): {r2:.2f} (Um valor negativo indica que o modelo não tem poder preditivo adequado.)
+                    - Erro Absoluto Médio (MAE): {mae:.2f}
+                    - Erro Quadrático Médio (MSE): {mse:.2f}
 
-            **Considerações para Apostas:**
-            Dada a baixa confiabilidade das previsões deste modelo, recomendamos cautela ao usar estas informações para suas decisões. Esses números devem ser vistos como estimativas, e é crucial considerar outras fontes e fatores ao tomar decisões baseadas nessas previsões.
-        """, unsafe_allow_html=True)
+                    **Considerações para Apostas:**
+                    Dada a baixa confiabilidade das previsões deste modelo, recomendamos cautela ao usar estas informações para suas decisões. Esses números devem ser vistos como estimativas, e é crucial considerar outras fontes e fatores ao tomar decisões baseadas
 
 
 with tab2:
